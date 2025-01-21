66512c5f74b90ceb55399d372de7938802def410
Khai Nguyen <khaivnguyen02@gmail.com>
2025-01-21 13:34:27 -0800

    Add function to get computer choice

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Hello World")
+function getComputerChoice() {
+  const computerChoice = Math.floor(Math.random() * 3);
+  if (computerChoice === 0) {
+    return "rock";
+  } else if (computerChoice === 1) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
