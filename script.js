09e3ef3108d6cb8074dc20cf1ffa4ac4521fa89e
Khai Nguyen <khaivnguyen02@gmail.com>
2025-01-21 13:55:58 -0800

    Add game logic to play a single round

--- script.js
+++ script.js
@@ -17,3 +17,47 @@ function getHumanChoice() {
     return "Invalid choice";
   }
 }
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice == computerChoice) {
+    console.log("It's a tie!")
+  }
+
+  if (humanChoice === "rock") {
+    if (computerChoice === "scissors") {
+      humanScore++;
+      console.log("You win! Rock beats Scissors");
+    } else {
+      computerScore++;
+      console.log("You lose! Paper beats Rock");
+    }
+  }
+
+  if (humanChoice === "paper") {
+    if (computerChoice === "rock") {
+      humanScore++;
+      console.log("You win! Paper beats Rock");
+    } else {
+      computerScore++;
+      console.log("You lose! Scissors beats Paper");
+    }
+  }
+  
+  if (humanChoice === "scissors") {
+    if (computerChoice === "paper") {
+      humanScore++;
+      console.log("You win! Scissors beats Paper");
+    } else {
+      computerScore++;
+      console.log("You lose! Rock beats Scissors");
+    }
+  }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
